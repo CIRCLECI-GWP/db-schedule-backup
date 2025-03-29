25a952a3abfca19eb9a8074588826494ed5103ea
yemiwebby <yemiwebby@gmail.com>
2025-03-29 00:12:16 +0000

    update schedule config

--- backup.js
+++ backup.js
@@ -2,22 +2,16 @@ require("dotenv").config();
 const exec = require("child_process").exec;
 const path = require("path");
 
-const {
-  BlobServiceClient,
-  StorageSharedKeyCredential,
-} = require("@azure/storage-blob");
+const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob");
 
 const backupDirPath = path.join(__dirname, "database-backup");
 
 const storeFileOnAzure = async (file) => {
   const account = process.env.ACCOUNT_NAME;
   const accountKey = process.env.ACCOUNT_KEY;
-  const containerName = "files";
+  const containerName = "dbsnapshots";
 
-  const sharedKeyCredential = new StorageSharedKeyCredential(
-    account,
-    accountKey
-  );
+  const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
 
   // instantiate
   const blobServiceClient = new BlobServiceClient(
@@ -26,21 +20,18 @@ const storeFileOnAzure = async (file) => {
   );
 
   const container = blobServiceClient.getContainerClient(containerName);
-  const blobName = "backup.bson";
+  const blobName = "companies.bson";
 
   const blockBlobClient = container.getBlockBlobClient(blobName);
 
   const uploadBlobResponse = await blockBlobClient.uploadFile(file);
-  console.log(
-    `Upload block blob ${blobName} successfully`,
-    uploadBlobResponse.requestId
-  );
+  console.log(`Upload block blob ${blobName} successfully`, uploadBlobResponse.requestId);
 };
 
 let cmd = `mongodump --out=${backupDirPath} --uri=${process.env.MONGODB_URI} --forceTableScan`;
 
 const dbAutoBackUp = () => {
-  let filePath = backupDirPath + `/companiesdb/companies.bson`;
+  let filePath = backupDirPath + `/companydb/companies.bson`;
 
   exec(cmd, (error, stdout, stderr) => {
     console.log([cmd, error, backupDirPath]);
